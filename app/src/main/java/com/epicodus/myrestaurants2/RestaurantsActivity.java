e69b328b8f92d3292d4ba5bb499500c2aa282d2c
Haneen AbuKhater and Kate Byars <haneen+kate@>
2017-10-09 14:28:47 -0700

    add new activity page

--- app/src/main/java/com/epicodus/myrestaurants2/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants2/RestaurantsActivity.java
@@ -1,13 +1,21 @@
 package com.epicodus.myrestaurants2;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
+    private TextView mLocationTextView;
+
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        mLocationTextView= (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the locations near: " + location);
     }
 }

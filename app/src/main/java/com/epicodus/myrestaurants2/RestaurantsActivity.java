f7c963938098b99cb7c7f2a95d4c66840b92415c
Haneen AbuKhater and Kate Byars <haneen+kate@>
2017-10-09 16:41:32 -0700

    last commit of the day.

--- app/src/main/java/com/epicodus/myrestaurants2/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants2/RestaurantsActivity.java
@@ -43,13 +43,13 @@ public class RestaurantsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-                Log.v(TAG, "In the onItemClickListener!");
+
             }
         });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the locations near: " + location);
-        Log.d("RestaurantsActivity", "In the onCreate method!");
+
     }
 }

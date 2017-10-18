253efd6c5904a4e8c3e361e90206af265ba3db6f
Guest User <Guest@Epicodus-3A.local>
2017-10-18 11:45:26 -0700

    add pager adapter logic.

--- app/src/main/java/com/epicodus/myrestaurants2/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants2/adapters/RestaurantListAdapter.java
@@ -1,5 +1,6 @@
 package com.epicodus.myrestaurants2.adapters;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epicodus.myrestaurants2.R;
+import com.epicodus.myrestaurants2.RestaurantDetailActivity;
 import com.epicodus.myrestaurants2.models.Restaurant;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -56,6 +60,15 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+            mContext.startActivity(intent);
         }
 
         public void bindRestaurant(Restaurant restaurant) {
